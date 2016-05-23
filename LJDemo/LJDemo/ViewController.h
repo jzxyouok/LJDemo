2d1a1e6ebd60e054ea1d5e3587492bb620d3c3fc
huanghuacai <18811108252@163.com>
2016-05-23 21:21:25 +0800

    初始化项目

---
+++ LJDemo/LJDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LJDemo
+//
+//  Created by 蔡万鸿 on 16/5/23.
+//  Copyright © 2016年 黄花菜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
