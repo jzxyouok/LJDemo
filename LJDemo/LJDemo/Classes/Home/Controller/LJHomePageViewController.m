0ff5bab914fa7fa7ba2d7ab241627163124709f8
huanghuacai <18811108252@163.com>
2016-05-28 13:24:01 +0800

    完成顶部图片下拉方法效果

--- LJDemo/LJDemo/Classes/Home/Controller/LJHomePageViewController.m
+++ LJDemo/LJDemo/Classes/Home/Controller/LJHomePageViewController.m
@@ -8,9 +8,8 @@
 
 #import "LJHomePageViewController.h"
 
-#define ImageHight 211.0f
-
-#define NavigationBarHight 64.0f
+/** 顶部图片高度 */
+static const CGFloat LJImageHeight = 192;
 
 @interface LJHomePageViewController ()<UITableViewDelegate,UITableViewDataSource>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
@@ -23,11 +22,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    //隐藏导航栏
+    self.navigationController.navigationBar.hidden = YES;
+    
     //4.设置contentInset属性（上左下右 的值）
-    self.tableView.contentInset = UIEdgeInsetsMake(ImageHight, 0, 0, 0);
+    self.tableView.contentInset = UIEdgeInsetsMake(LJImageHeight, 0, 0, 0);
     
     _zoomImageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"homepage_visual"]];
-    _zoomImageView.frame = CGRectMake(0, -ImageHight, self.view.frame.size.width, ImageHight);
+    _zoomImageView.frame = CGRectMake(0, -LJImageHeight, self.view.frame.size.width, LJImageHeight);
     
     //contentMode = UIViewContentModeScaleAspectFill时，高度改变宽度也跟着改变
     _zoomImageView.contentMode = UIViewContentModeScaleAspectFill;//重点（不设置那将只会被纵向拉伸）
@@ -39,10 +41,11 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - UIScrollViewDelegate
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView{
     
     CGFloat y = scrollView.contentOffset.y;//根据实际选择加不加上NavigationBarHight（44、64 或者没有导航条）
-    if (y < -ImageHight) {
+    if (y < -LJImageHeight) {
         CGRect frame = _zoomImageView.frame;
         frame.origin.y = y;
         frame.size.height =  -y;//contentMode = UIViewContentModeScaleAspectFill时，高度改变宽度也跟着改变
--- LJDemo/LJDemo/Classes/Me/Controller/LJMyInfoViewController.m
+++ LJDemo/LJDemo/Classes/Me/Controller/LJMyInfoViewController.m
@@ -25,19 +25,16 @@
 
 #import "LJMyInfoTopView.h"
 
-
-#define ImageHight 200.0f
-
-#define NavigationBarHight 64.0f
+/** 顶部图片高度 */
+static const CGFloat LJImageHeight = 235;
 
 @interface LJMyInfoViewController ()<UITableViewDelegate,UITableViewDataSource>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 /** 数据源 */
 @property (nonatomic, strong) NSMutableArray *dataSourceArray;
-
-@property (nonatomic, strong)  UIImageView *zoomImageView;//变焦图片做底层
-
-// 表示组的集合
+/** 变焦图片做底层 */
+@property (nonatomic, strong)  UIImageView *zoomImageView;
+/** 表示组的集合 */
 @property (nonatomic, strong) NSMutableArray *groups;
 @end
 
@@ -47,106 +44,42 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    //隐藏导航栏
+    self.navigationController.navigationBar.hidden = YES;
+    
     //4.设置contentInset属性（上左下右 的值）
-    self.tableView.contentInset = UIEdgeInsetsMake(ImageHight, 0, 0, 0);
+    self.tableView.contentInset = UIEdgeInsetsMake(LJImageHeight, 0, 0, 0);
     
     _zoomImageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"bg"]];
-    _zoomImageView.frame = CGRectMake(0, -ImageHight, self.view.frame.size.width, ImageHight);
+    _zoomImageView.frame = CGRectMake(0, -LJImageHeight, self.view.frame.size.width, LJImageHeight);
     
     //contentMode = UIViewContentModeScaleAspectFill时，高度改变宽度也跟着改变
     _zoomImageView.contentMode = UIViewContentModeScaleAspectFill;//重点（不设置那将只会被纵向拉伸）
     [self.tableView addSubview:_zoomImageView];
     
+    //初始化数据
     [self loadGroup0];
-    
     [self loadGroup1];
-    
     [self loadGroup2];
-    
     [self loadGroup3];
-    
     [self loadGroup4];
-    
     [self loadGroup5];
 }
 
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView{
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
     
-    CGFloat y = scrollView.contentOffset.y+NavigationBarHight;//根据实际选择加不加上NavigationBarHight（44、64 或者没有导航条）
-    if (y < -ImageHight) {
+}
+
+#pragma mark - UIScrollViewDelegate
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    CGFloat y = scrollView.contentOffset.y;//根据实际选择加不加上NavigationBarHight（44、64 或者没有导航条）
+    if (y < -LJImageHeight) {
         CGRect frame = _zoomImageView.frame;
         frame.origin.y = y;
         frame.size.height =  -y;//contentMode = UIViewContentModeScaleAspectFill时，高度改变宽度也跟着改变
         _zoomImageView.frame = frame;
     }
-}
-
-- (void)loadGroup0 {
-    // 创建组, 以及每组内的模型
-    LJGroup *group0 = [[LJGroup alloc] init];
-    group0.items = @[@"group0"];
-    [self.groups addObject:group0];
-}
-
-- (void)loadGroup1 {
-    // 创建组, 以及每组内的模型
-    LJGroup *group1 = [[LJGroup alloc] init];
-    
-    LJItemArrow *item1_0 = [LJItemArrow itemArrowWithTitle:@"我关注的新房" icon:@"myhome_icon_xinfang" numStr:@"0" targetVc:[LJFollowedBuildingListViewController class]];
-    LJItemArrow *item1_1 = [LJItemArrow itemArrowWithTitle:@"我关注的二手房/租房" icon:@"myhome_icon_ershoufang" numStr:@"0" targetVc:[LJMyFollowedHouseListViewController class]];
-    LJItemArrow *item1_2 = [LJItemArrow itemArrowWithTitle:@"我关注的小区" icon:@"myhome-icon-community" numStr:@"0" targetVc:[LJMyFollowedCommunityViewController class]];
-    LJItemArrow *item1_3 = [LJItemArrow itemArrowWithTitle:@"我的看房记录" icon:@"myhome-icon-record" numStr:@"0" targetVc:[LJMySeenRecordViewController class]];
-    
-    group1.items = @[item1_0, item1_1, item1_2, item1_3];
-    [self.groups addObject:group1];
-}
-
-- (void)loadGroup2 {
-    // 创建组, 以及每组内的模型
-    LJGroup *group2 = [[LJGroup alloc] init];
-    
-    LJItemArrow *item2_0 = [LJItemArrow itemArrowWithTitle:@"我的问答" icon:@"icon_wenda" numStr:@"0" targetVc:[LJMyAskViewController class]];
-    
-    group2.items = @[item2_0];
-    [self.groups addObject:group2];
-}
-
-- (void)loadGroup3 {
-    // 创建组, 以及每组内的模型
-    LJGroup *group3 = [[LJGroup alloc] init];
-    
-    LJItemArrow *item3_0 = [LJItemArrow itemArrowWithTitle:@"我是卖家" icon:@"my_delegate" numStr:@"0" targetVc:[LJSellerEntrancePageController class]];
-    
-    group3.items = @[item3_0];
-    [self.groups addObject:group3];
-}
-
-- (void)loadGroup4 {
-    // 创建组, 以及每组内的模型
-    LJGroup *group4 = [[LJGroup alloc] init];
-    
-    LJItemArrow *item4_0 = [LJItemArrow itemArrowWithTitle:@"邀请好友" icon:@"wd_icon_Invitefriends" numStr:@"" targetVc:[LJInviteFriendsViewController class]];
-    LJItemArrow *item4_1 = [LJItemArrow itemArrowWithTitle:@"购房计算器" icon:@"myhome-icon-cal" numStr:@"" targetVc:[LJCalculateViewController class]];
-    LJItemArrow *item4_2 = [LJItemArrow itemArrowWithTitle:@"用户反馈" icon:@"myhome-icon-feedback" numStr:@"" targetVc:[LCUserFeedbackViewController class]];
-    LJItemArrow *item4_3 = [LJItemArrow itemArrowWithTitle:@"客服电话" icon:@"ic_customerservice" numStr:@"10109666" targetVc:nil];
-    
-    group4.items = @[item4_0, item4_1, item4_2, item4_3];
-    [self.groups addObject:group4];
-}
-
-- (void)loadGroup5 {
-    // 创建组, 以及每组内的模型
-    LJGroup *group5 = [[LJGroup alloc] init];
-    
-    LJItemArrow *item5_0 = [LJItemArrow itemArrowWithTitle:@"设置" icon:@"myhome-icon-setting" numStr:@"" targetVc:[LJSettingViewController class]];
-    
-    group5.items = @[item5_0];
-    [self.groups addObject:group5];
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-
 }
 
 #pragma mark - UITableViewDataSource
@@ -215,6 +148,71 @@
     return 44;
 }
 
+#pragma mark - Private
+- (void)loadGroup0 {
+    // 创建组, 以及每组内的模型
+    LJGroup *group0 = [[LJGroup alloc] init];
+    group0.items = @[@"group0"];
+    [self.groups addObject:group0];
+}
+
+- (void)loadGroup1 {
+    // 创建组, 以及每组内的模型
+    LJGroup *group1 = [[LJGroup alloc] init];
+    
+    LJItemArrow *item1_0 = [LJItemArrow itemArrowWithTitle:@"我关注的新房" icon:@"myhome_icon_xinfang" numStr:@"0" targetVc:[LJFollowedBuildingListViewController class]];
+    LJItemArrow *item1_1 = [LJItemArrow itemArrowWithTitle:@"我关注的二手房/租房" icon:@"myhome_icon_ershoufang" numStr:@"0" targetVc:[LJMyFollowedHouseListViewController class]];
+    LJItemArrow *item1_2 = [LJItemArrow itemArrowWithTitle:@"我关注的小区" icon:@"myhome-icon-community" numStr:@"0" targetVc:[LJMyFollowedCommunityViewController class]];
+    LJItemArrow *item1_3 = [LJItemArrow itemArrowWithTitle:@"我的看房记录" icon:@"myhome-icon-record" numStr:@"0" targetVc:[LJMySeenRecordViewController class]];
+    
+    group1.items = @[item1_0, item1_1, item1_2, item1_3];
+    [self.groups addObject:group1];
+}
+
+- (void)loadGroup2 {
+    // 创建组, 以及每组内的模型
+    LJGroup *group2 = [[LJGroup alloc] init];
+    
+    LJItemArrow *item2_0 = [LJItemArrow itemArrowWithTitle:@"我的问答" icon:@"icon_wenda" numStr:@"0" targetVc:[LJMyAskViewController class]];
+    
+    group2.items = @[item2_0];
+    [self.groups addObject:group2];
+}
+
+- (void)loadGroup3 {
+    // 创建组, 以及每组内的模型
+    LJGroup *group3 = [[LJGroup alloc] init];
+    
+    LJItemArrow *item3_0 = [LJItemArrow itemArrowWithTitle:@"我是卖家" icon:@"my_delegate" numStr:@"0" targetVc:[LJSellerEntrancePageController class]];
+    
+    group3.items = @[item3_0];
+    [self.groups addObject:group3];
+}
+
+- (void)loadGroup4 {
+    // 创建组, 以及每组内的模型
+    LJGroup *group4 = [[LJGroup alloc] init];
+    
+    LJItemArrow *item4_0 = [LJItemArrow itemArrowWithTitle:@"邀请好友" icon:@"wd_icon_Invitefriends" numStr:@"" targetVc:[LJInviteFriendsViewController class]];
+    LJItemArrow *item4_1 = [LJItemArrow itemArrowWithTitle:@"购房计算器" icon:@"myhome-icon-cal" numStr:@"" targetVc:[LJCalculateViewController class]];
+    LJItemArrow *item4_2 = [LJItemArrow itemArrowWithTitle:@"用户反馈" icon:@"myhome-icon-feedback" numStr:@"" targetVc:[LCUserFeedbackViewController class]];
+    LJItemArrow *item4_3 = [LJItemArrow itemArrowWithTitle:@"客服电话" icon:@"ic_customerservice" numStr:@"10109666" targetVc:nil];
+    
+    group4.items = @[item4_0, item4_1, item4_2, item4_3];
+    [self.groups addObject:group4];
+}
+
+- (void)loadGroup5 {
+    // 创建组, 以及每组内的模型
+    LJGroup *group5 = [[LJGroup alloc] init];
+    
+    LJItemArrow *item5_0 = [LJItemArrow itemArrowWithTitle:@"设置" icon:@"myhome-icon-setting" numStr:@"" targetVc:[LJSettingViewController class]];
+    
+    group5.items = @[item5_0];
+    [self.groups addObject:group5];
+}
+
+#pragma mark - Lazy
 - (NSMutableArray *)groups {
     if (_groups == nil) {
         _groups = [NSMutableArray array];
