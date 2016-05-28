454eb5329fda903cc8d6c0ca3d4d891643689aba
huanghuacai <18811108252@163.com>
2016-05-28 09:56:08 +0800

    首页和消息页隐藏navBar

--- LJDemo/LJDemo/Classes/Main/Controller/LJTabBarController.m
+++ LJDemo/LJDemo/Classes/Main/Controller/LJTabBarController.m
@@ -7,9 +7,20 @@
 //
 
 #import "LJTabBarController.h"
-#import "LJHomeViewController.h"
-#import "LJMyInfoViewController.h"
-#import "LJMessageViewController.h"
+
+/**
+ *  该模块根控制器是否有导航栏
+ */
+typedef NS_ENUM(NSUInteger, LJMoudleNavState) {
+    /**
+     *  没有导航栏
+     */
+    LJMoudleNavStateNone,
+    /**
+     *  有导航栏
+     */
+    LJMoudleNavStateHave
+};
 
 @interface LJTabBarController ()
 
@@ -38,13 +49,13 @@
  */
 - (void)p_addChildViewControllers {
     // 首页
-    [self p_addChildVCWithSBName:@"Home" title:@"首页" norImageName:@"bottom-bar-home-icon" selectedImageName:@"bottom-bar-home-icon-active"];
+    [self p_addChildVCWithSBName:@"Home" title:@"首页" norImageName:@"bottom-bar-home-icon" selectedImageName:@"bottom-bar-home-icon-active" navState:LJMoudleNavStateNone];
 
     // 消息
-    [self p_addChildVCWithSBName:@"Message" title:@"消息" norImageName:@"bottom-bar-messege-icon" selectedImageName:@"bottom-bar-messege-icon-active"];
+    [self p_addChildVCWithSBName:@"Message" title:@"消息" norImageName:@"bottom-bar-messege-icon" selectedImageName:@"bottom-bar-messege-icon-active" navState:LJMoudleNavStateHave];
 
     // 我
-    [self p_addChildVCWithSBName:@"Me" title:@"我" norImageName:@"bottom-bar-center-icon" selectedImageName:@"bottom-bar-center-icon-active"];
+    [self p_addChildVCWithSBName:@"Me" title:@"我" norImageName:@"bottom-bar-center-icon" selectedImageName:@"bottom-bar-center-icon-active" navState:LJMoudleNavStateNone];
 }
 
 /**
@@ -57,29 +68,45 @@
  *
  *  @return 根控制器
  */
-- (UIViewController *)p_addChildVCWithSBName:(NSString *)sbName  title:(NSString *)title norImageName:(NSString *)norImageName selectedImageName:(NSString *)selectedImageName{
+- (UIViewController *)p_addChildVCWithSBName:(NSString *)sbName  title:(NSString *)title norImageName:(NSString *)norImageName selectedImageName:(NSString *)selectedImageName navState:(LJMoudleNavState)navState{
     // 1.加载Storyboard
     UIStoryboard *sb = [UIStoryboard storyboardWithName:sbName bundle:nil];
     // 2.创建Storyboard中的初始控制器
-    UINavigationController *nav = sb.instantiateInitialViewController;
-    // 3.调用addChildVCWithController
-    [self p_addChildVCWithController:nav.topViewController title:title norImageName:norImageName selectedImageName:selectedImageName];
-    return nav.topViewController;
+    if (navState == LJMoudleNavStateNone) {
+        UIViewController *vc = sb.instantiateInitialViewController;
+        // 3.调用addChildVCWithController
+        [self p_addChildVCWithController:vc title:title norImageName:norImageName selectedImageName:selectedImageName  navState:LJMoudleNavStateNone];
+        return vc;
+    }
+    else {
+        UINavigationController *nav = sb.instantiateInitialViewController;
+        // 3.调用addChildVCWithController
+        [self p_addChildVCWithController:nav.topViewController title:title norImageName:norImageName selectedImageName:selectedImageName  navState:LJMoudleNavStateHave];
+        return nav.topViewController;
+    }
 }
 
 /**
  *  设置指定控制器的相关属性
  */
-- (UIViewController *)p_addChildVCWithController:(UIViewController *)vc  title:(NSString *)title norImageName:(NSString *)norImageName selectedImageName:(NSString *)selectedImageName{
+- (UIViewController *)p_addChildVCWithController:(UIViewController *)vc  title:(NSString *)title norImageName:(NSString *)norImageName selectedImageName:(NSString *)selectedImageName  navState:(LJMoudleNavState)navState{
     // 设置标题
     vc.tabBarItem.title = title;
-    vc.navigationItem.title = title;
+    //导航栏标题
+    if (navState == LJMoudleNavStateHave) {
+        vc.navigationItem.title = title;
+    }
     // 设置默认图片
     vc.tabBarItem.image = [UIImage imageNamed:norImageName];
     // 设置选中图片
     vc.tabBarItem.selectedImage =  [UIImage imageNamed:selectedImageName];
     // 添加到父控件
-    [self addChildViewController:vc.navigationController];
+    if (navState == LJMoudleNavStateHave) {
+        [self addChildViewController:vc.navigationController];
+    }
+    else {
+        [self addChildViewController:vc];
+    }
     return vc;
 }
 
