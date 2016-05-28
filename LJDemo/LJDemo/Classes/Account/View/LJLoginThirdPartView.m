2bd503281f21c58b1c2f1e0a676d0459e747aef9
huanghuacai <18811108252@163.com>
2016-05-28 08:56:19 +0800

    登录界面初始化

---
+++ LJDemo/LJDemo/Classes/Account/View/LJLoginThirdPartView.m
@@ -0,0 +1,13 @@
+//
+//  LJLoginThirdPartView.m
+//  LJDemo
+//
+//  Created by 蔡万鸿 on 16/5/28.
+//  Copyright © 2016年 黄花菜. All rights reserved.
+//
+
+#import "LJLoginThirdPartView.h"
+
+@implementation LJLoginThirdPartView
+
+@end
