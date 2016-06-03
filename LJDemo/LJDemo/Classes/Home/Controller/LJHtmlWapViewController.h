e9d192b98e93589ecb0622d89f56be12aecec9aa
huanghuacai <18811108252@163.com>
2016-06-03 19:47:13 +0800

    完成估价页面绘制

---
+++ LJDemo/LJDemo/Classes/Home/Controller/LJHtmlWapViewController.h
@@ -0,0 +1,13 @@
+//
+//  LJHtmlWapViewController.h
+//  LJDemo
+//
+//  Created by 蔡万鸿 on 16/6/3.
+//  Copyright © 2016年 黄花菜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LJHtmlWapViewController : UIViewController
+
+@end
---
+++ LJDemo/LJDemo/Classes/Home/Controller/LJPEHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  LJPEHomeViewController.h
+//  LJDemo
+//
+//  Created by 蔡万鸿 on 16/6/3.
+//  Copyright © 2016年 黄花菜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LJPEHomeViewController : UIViewController
+
+@end
